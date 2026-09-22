059b5fad3a7ca8d6619bfee21ed7c94588e752f1
niklaushirt <niklaushirt@gmail.com>
2026-09-22 07:23:52 +0200

    Initial

---
+++ tools/97_addons/ibm-aiops-demo-ui/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+podman machine start
+
+podman login quay.io -u niklaushirt@gmail.com
+
+cd tools/97_addons/ibm-aiops-demo-ui/demoui
+
+
+export CONT_VERSION=5.1.2
+podman buildx build --platform linux/amd64 -t quay.io/niklaushirt/ibm-aiops-demo-ui:$CONT_VERSION --load -f=Containerfile_small
+podman push quay.io/niklaushirt/ibm-aiops-demo-ui:$CONT_VERSION
